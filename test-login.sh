f39620be27f9cd471e43642e7847e46296d39053
Bruno <brunoto02028@gmail.com>
2026-02-27 15:54:05 +0000

    chore: remove temp scripts from tracking

--- test-login.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-curl -s -X POST http://localhost:3100/api/auth/send-login-code \
-  -H 'Content-Type: application/json' \
-  -d '{"email":"brunotoaz@gmail.com","password":"test123"}' 2>&1
-echo ""
-echo "--- LOGS ---"
-pm2 logs homeledger --lines 10 --nostream 2>&1
